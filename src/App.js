114c147ebe8665f337d1754b2d9180a964da4e81
Ruben9922 <Ruben9922@users.noreply.github.com>
2020-11-24 22:44:39 +0000

    Create initial implementation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,156 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Container from "@material-ui/core/Container";
+import Header from "./Header";
+import NumberFormat from 'react-number-format';
+import TextField from "@material-ui/core/TextField";
+import {makeStyles} from "@material-ui/core/styles";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import TableContainer from "@material-ui/core/TableContainer";
+import Paper from "@material-ui/core/Paper";
+import Table from "@material-ui/core/Table";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
+import TableBody from "@material-ui/core/TableBody";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const useStyles = makeStyles((theme) => ({
+    root: {
+        '& > *': {
+            margin: theme.spacing(1),
+        },
+    },
+}));
+
+function NumberFormatCustom(props) {
+    const {inputRef, onChange, ...other} = props;
+
+    return (
+        <NumberFormat
+            {...other}
+            getInputRef={inputRef}
+            onValueChange={(values) => {
+                onChange({
+                    target: {
+                        name: props.name,
+                        value: values.value,
+                    },
+                });
+            }}
+            thousandSeparator
+            isNumericString
+            decimalScale={0}
+            // prefix="¤ "
+        />
+    );
 }
 
-export default App;
+function calculate(initialAmount, recurringAmount, growth, yearCount) {
+    const monthlyGrowth = Math.pow(1 + growth, 1 / 12) - 1;
+
+    let valueByYear = [];
+    valueByYear.push(initialAmount);
+    for (let i = 1; i < yearCount; i++) {
+        let value = valueByYear[i - 1];
+        for (let j = 0; j < 12; j++) {
+            value += recurringAmount;
+            value *= 1 + monthlyGrowth;
+        }
+        valueByYear.push(value);
+    }
+    return valueByYear;
+}
+
+export default function App() {
+    const classes = useStyles();
+
+    // const [initialAmount, setInitialAmount] = React.useState(0);
+    // const [recurringAmount, setRecurringAmount] = React.useState(0);
+    // const [growth, setGrowth] = React.useState(0.1);
+    const [inputs, setInputs] = React.useState({
+        initialAmount: "20000",
+        recurringAmount: "500",
+        growth: "10",
+        yearCount: "50",
+    });
+
+    const handleChange = event => {
+        setInputs({...inputs, [event.target.name]: event.target.value});
+    };
+
+    let valueByYear = calculate(parseFloat(inputs.initialAmount), parseFloat(inputs.recurringAmount),
+        parseFloat(inputs.growth) / 100, parseInt(inputs.yearCount));
+
+    return [
+        <Header/>,
+        <Container maxWidth="md">
+            <div className={classes.root}>
+                <TextField
+                    label="Initial amount"
+                    value={inputs.initialAmount}
+                    onChange={handleChange}
+                    name="initialAmount"
+                    InputProps={{
+                        startAdornment: <InputAdornment position="start">$</InputAdornment>,
+                        inputComponent: NumberFormatCustom,
+                    }}
+                    variant="outlined"
+                />
+                <TextField
+                    label="Monthly amount"
+                    value={inputs.recurringAmount}
+                    onChange={handleChange}
+                    name="recurringAmount"
+                    InputProps={{
+                        startAdornment: <InputAdornment position="start">$</InputAdornment>,
+                        inputComponent: NumberFormatCustom,
+                    }}
+                    variant="outlined"
+                />
+                <TextField
+                    label="Annual growth"
+                    value={inputs.growth}
+                    onChange={handleChange}
+                    name="growth"
+                    InputProps={{
+                        endAdornment: <InputAdornment position="end">%</InputAdornment>,
+                    }}
+                    variant="outlined"
+                />
+                <TextField
+                    label="Years"
+                    value={inputs.yearCount}
+                    onChange={handleChange}
+                    name="yearCount"
+                    variant="outlined"
+                />
+            </div>
+            <TableContainer component={Paper}>
+                <Table className={classes.table} size="small" aria-label="a dense table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Year</TableCell>
+                            <TableCell align="right">Value</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {valueByYear.map((value, index) => (
+                            <TableRow key={index}>
+                                <TableCell component="th" scope="row">
+                                    {`Year ${index + 1}`}
+                                </TableCell>
+                                <TableCell align="right">
+                                    <NumberFormat
+                                        value={value}
+                                        displayType="text"
+                                        thousandSeparator
+                                        isNumericString
+                                        decimalScale={0}/>
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </Container>
+    ];
+}
