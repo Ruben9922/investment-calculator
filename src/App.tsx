a990af49f21c5db45d273f9be6f72962fce36335
Ruben Dougall <Ruben9922@users.noreply.github.com>
2024-03-11 22:23:43 +0000

    Make alert closeable

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import {Box, Container} from "@mui/material";
 import Alert from '@mui/material/Alert';
 import AlertTitle from '@mui/material/AlertTitle';
 import CssBaseline from "@mui/material/CssBaseline";
+import Stack from '@mui/material/Stack';
 import {useState} from "react";
 import Form from "./Form.tsx";
 import Header from "./Header.tsx";
 import Table from "./Table.tsx";
-import Stack from '@mui/material/Stack';
 
 function calculate(initialAmount: number, recurringAmount: number, growth: number, yearCount: number): number[] {
     const multiplier = 1 + growth;
@@ -30,6 +30,7 @@ function App() {
     const [recurringAmountString, setRecurringAmountString] = useState("500");
     const [growthString, setGrowthString] = useState("10");
     const [yearCountString, setYearCountString] = useState("50");
+    const [isAlertShown, setIsAlertShown] = useState(true);
 
     const initialAmount = parseFloat(initialAmountString);
     const recurringAmount = parseFloat(recurringAmountString);
@@ -43,10 +44,12 @@ function App() {
             <Header />
             <Container maxWidth="md" component={Box} padding={4}>
                 <Stack spacing={4}>
-                <Alert severity="warning">
-                    <AlertTitle>Disclaimer</AlertTitle>
-                    This is for indicative purposes only and should not be used as the basis for any investment decision. The data shown is purely hypothetical, based on the parameters entered, and does not necessarily reflect real-world investing. It does not take into account inflation, taxes, fees or other factors that may affect the value of your investment. I do not make any guarantees regarding the accuracy of the data shown.
-                </Alert>
+                    {isAlertShown && (
+                        <Alert severity="warning" onClose={() => setIsAlertShown(false)}>
+                            <AlertTitle>Disclaimer</AlertTitle>
+                            This is for indicative purposes only and should not be used as the basis for any investment decision. The data shown is purely hypothetical, based on the parameters entered, and does not necessarily reflect real-world investing. It does not take into account inflation, taxes, fees or other factors that may affect the value of your investment. I do not make any guarantees regarding the accuracy of the data shown.
+                        </Alert>
+                    )}
 
                 <Form
                     initialAmountString={initialAmountString}
