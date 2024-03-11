06b5a3ee76d1776ddc8e64e2ba417ebcc7946e33
Ruben Dougall <Ruben9922@users.noreply.github.com>
2024-03-11 21:07:43 +0000

    Create new Vite project (React & TypeScript)

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,35 @@
-import {Container, VStack} from "@chakra-ui/react";
-import {useState} from 'react'
-import Form from "./Form.tsx";
-import Table from "./Table.tsx";
-
-function calculate(initialAmount: number, recurringAmount: number, growth: number, yearCount: number): number[] {
-    const multiplier = 1 + growth;
-    const monthlyMultiplier = multiplier ** (1 / 12);
-
-    const valuesByYear: number[] = [];
-    valuesByYear.push(initialAmount);
-    for (let i = 1; i <= yearCount; i++) {
-        let value = valuesByYear[i - 1];
-        for (let j = 0; j < 12; j++) {
-            value += recurringAmount;
-            value *= monthlyMultiplier;
-        }
-        valuesByYear.push(value);
-    }
-    return valuesByYear;
-}
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-    const [initialAmountString, setInitialAmountString] = useState("20000");
-    const [recurringAmountString, setRecurringAmountString] = useState("500");
-    const [growthString, setGrowthString] = useState("10");
-    const [yearCountString, setYearCountString] = useState("50");
+  const [count, setCount] = useState(0)
 
-    const initialAmount = parseFloat(initialAmountString);
-    const recurringAmount = parseFloat(recurringAmountString);
-    const growth = parseFloat(growthString) / 100;
-    const yearCount = parseInt(yearCountString);
-    const valuesByYear = calculate(initialAmount, recurringAmount, growth, yearCount);
-
-    return (
-        <Container maxWidth="container.md" padding={10}>
-            <VStack gap={10} alignItems="stretch">
-                <Form
-                    initialAmountString={initialAmountString}
-                    recurringAmountString={recurringAmountString}
-                    growthString={growthString}
-                    yearCountString={yearCountString}
-                    setInitialAmountString={setInitialAmountString}
-                    setRecurringAmountString={setRecurringAmountString}
-                    setGrowthString={setGrowthString}
-                    setYearCountString={setYearCountString}
-                />
-
-                <Table valuesByYear={valuesByYear} />
-            </VStack>
-        </Container>
-    )
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
 export default App
