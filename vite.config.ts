5f8ad48635dcb7bdd92703524e43c0cdc3f1a68c
Ruben Dougall <Ruben9922@users.noreply.github.com>
2024-03-11 22:57:07 +0000

    Remove unneeded origin from base public path in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import {defineConfig} from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://rubendougall.co.uk/projects/investment-calculator/",
+  base: "/projects/investment-calculator/",
 })
