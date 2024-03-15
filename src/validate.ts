786f4bfdf7ac3e933d9401995f1df3d48fb38302
Ruben Dougall <Ruben9922@users.noreply.github.com>
2024-03-15 14:18:46 +0000

    Refactor validation & improve number formatting

--- src/validate.ts
+++ src/validate.ts
@@ -1,37 +1,34 @@
-// function generateErrorMessage(fieldName: string, min: number, max: number): string {
-//     return `Must be between ${min} and ${max} (inclusive).`
-// }
+function formatNumber(value: number): string {
+    return value >= 1_000_000
+        ? value.toExponential()
+            .replace("e", "×10^")
+            .replace("1×", "")
+            .replace("+", "")
+        : value.toString(); // TODO: Add thousands separator
+}
 
-export function validateInitialAmount(initialAmount: number): string | null {
-    if (initialAmount <= 0 || initialAmount > 1_000_000_000_000) {
-        return "Must be greater than zero and less than (or equal to) 1,000,000,000,000"
+function validateNumber(value: number, min: number, max: number): string | null {
+    if (value < min || value > max) {
+        return `Must be between ${formatNumber(min)} and ${formatNumber(max)} (inclusive).`
     }
 
     return null;
+}
+
+export function validateInitialAmount(initialAmount: number): string | null {
+    return validateNumber(initialAmount, 1, 1_000_000_000_000)
 }
 
 export function validateRecurringAmount(recurringAmount: number): string | null {
-    if (recurringAmount <= 0 || recurringAmount > 1_000_000_000_000) {
-        return "Must be greater than zero and less than (or equal to) 1,000,000,000,000"
-    }
-
-    return null;
+    return validateNumber(recurringAmount, 1, 1_000_000_000_000)
 }
 
 export function validateGrowth(growth: number): string | null {
-    if (growth <= 0 || growth > 1_000_000) {
-        return "Must be greater than zero and less than (or equal to) 1,000,000"
-    }
-
-    return null;
+    return validateNumber(growth, 1, 1_000_000);
 }
 
 export function validateYearCount(yearCount: number): string | null {
-    if (yearCount <= 0 || yearCount > 100) {
-        return "Must be greater than zero and less than (or equal to) 100"
-    }
-
-    return null;
+    return validateNumber(yearCount, 1, 100);
 }
 
 export function validate(initialAmount: number, recurringAmount: number, growth: number, yearCount: number): boolean {
