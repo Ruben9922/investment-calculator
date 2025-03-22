50353bc5330c934b032f1afb00392e94911820a8
Ruben Dougall <Ruben9922@users.noreply.github.com>
2025-03-22 13:09:33 +0000

    Fix imports

--- src/mortgages/MortgagesTable.tsx
+++ src/mortgages/MortgagesTable.tsx
@@ -8,7 +8,7 @@ import TableRow from "@mui/material/TableRow";
 import {useContext} from "react";
 import {CurrencyContext} from "../App.tsx";
 import {formatNumberForTable, formatPercent} from "../validate.ts";
-import MortgageYearData from "./models.ts";
+import {MortgageYearData} from "./models.ts";
 
 type MortgagesTableProps = {
     yearsData: MortgageYearData[];
